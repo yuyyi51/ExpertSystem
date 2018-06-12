2969fefd4a72334f7d3c6815ec13a1ea0d3fb4c7
轮回 <c_h_e_n123@163.com>
2018-06-13 00:31:48 +0800

    完善注册功能

--- public/js/register.js
+++ public/js/register.js
@@ -15,12 +15,43 @@ socket.on('user:register', (res) => {
     }
 });
 
+$$('uname').onfocus=()=>{
+    $$('uname_note').style.visibility="visible";
+};
+$$('uname').onblur=()=>{
+    $$('uname_note').style.visibility="hidden";
+};
+
+$$('psw').onfocus=()=>{
+    $$('pword_note').style.visibility="visible";
+};
+$$('psw').onblur=()=>{
+    $$('pword_note').style.visibility="hidden";
+};
+
+$$('email').onfocus=()=>{
+    $$('email_note').style.visibility="visible";
+};
+$$('email').onblur=()=>{
+    $$('email_note').style.visibility="hidden";
+};
+
 $$('regisform').onsubmit = (event) => {
     let authinfo = {
         user: $$('uname').value,
         password: SparkMD5.hash($$('psw').value),
         email: $$('email').value
     };
+    if(authinfo.user.toString().length>12||authinfo.user.toString().length<4)
+    {
+        alert("用户名不合规范，请重新输入");
+        return false;
+    }
+    if($$('psw').value.length>14||$$('psw').value.length<6)
+    {
+        alert("密码不合规范，请重新输入");
+        return false;
+    }
     socket.emit('user:register', authinfo);
     return false;
 };
