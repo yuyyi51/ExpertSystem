ab3e5e2891746f94ff8de226d66185da1988a9d6
迷途的指针 <yuyyi51@126.com>
2018-06-15 21:35:19 +0800

    把一些页面加进项目

--- public/test/javascripts/page2.js
+++
@@ -1,6 +0,0 @@
-const socket = io.connect();
-function $$(id) { return document.getElementById(id); }
-
-
-console.log(cookie_helper.getCookie('expert_system_username'));
-console.log(cookie_helper.getCookie('expert_system_password'));
